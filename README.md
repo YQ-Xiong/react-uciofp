ed38cb2cc7f8b38460c74ec4bf38f0cf6d16d23a
YQ-Xiong <aydenxiong@hotmail.com>
2021-07-26 17:11:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-uciofp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-uciofp)
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+import DataGrid, {
+  Column,
+  ColumnChooser,
+  Grouping,
+  GroupPanel,
+  Pager,
+  Paging,
+  SearchPanel,
+  checkBoxesMode,
+  Selection,
+  Export,
+  Editing,
+  FilterRow,
+  MasterDetail,
+  HeaderFilter
+} from 'devextreme-react/data-grid';
+
+
+export default function App() {
+  return (
+    <div>
+      <h4>Example</h4>
+      <DataGrid>
+        
+      </DataGrid>
+    </div>
+  );
+}
